da17364aae7050f942f0ba2223b638d381a88613
Pedro Nadotti <nadottipedro5@gmail.com>
2025-03-10 14:45:15 -0300

    Tentando fazer o primeiro teste para imoveis filtrados por cidade

--- test_server.py
+++ test_server.py
@@ -19,3 +19,11 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.get_json() == {"mensagem": "Conexao bem sucedida"}
+
+#criando teste para rota filtrando por cidade
+
+def test_por_cidade(client):
+    response = client.get('/por-cidade/Limeira')
+    assert response.status_code == 200
+    filtrado = response.get_json()
+    assert filtrado["cidades"] == "Limeira"
