d8e2e12b7e6cc6f6c16b44e1a303d694a92b0d87
Pedro Nadotti <nadottipedro5@gmail.com>
2025-03-12 11:03:13 -0300

    Melhorando o teste de filtro de cidades

--- server.py
+++ server.py
@@ -24,25 +24,14 @@ def conectando_db():
 def index():
     conexao = conectando_db()
     if conexao:
-        return jsonify({'mensagem': f'{conexao}'})
+        return jsonify({'mensagem': 'ta dando certo hehehehe'})
     
     else:
         return jsonify({'erro': f'{conexao}'}), 500
     
 @app.route('/cidade', methods=['POST', 'GET'])
 def por_cidade():
-    connection = conectando_db()
-    cursor = connection.cursor(dictionary=True)
-    
-
-    cidade_selecionada = request.form.get('cidade')
-    
-    if cidade_selecionada:
-        cursor.execute(f"SELECT * FROM MyImoveis WHERE cidade = {cidade_selecionada}")
-    else:
-        cursor.execute("SELECT * FROM MyImoveis")
-
-    return jsonify({"mensagem": "bagulho"})
+    return jsonify({"mensagem": "bagulho"}), 200
 
 if __name__ =='__main__':
     app.run(debug=True)
--- test_server.py
+++ test_server.py
@@ -1,4 +1,6 @@
 import pytest
+from unittest.mock import patch, MagicMock
+from server import app, conectando_db
 
 @pytest.fixture()
 def app():
@@ -21,9 +23,35 @@ def test_index(client):
     assert response.get_json() == {"mensagem": "Conexao bem sucedida"}
 
 #criando teste para rota filtrando por cidade
+@patch('server.conectando_db')
+def test_por_cidade(mock_conectando_db, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
 
-def test_por_cidade(client):
-    response = client.get('/por-cidade/Limeira')
+    # Configuramos o Mock para retornar o cursor quando chamarmos conn.cursor()
+    mock_conn.cursor.return_value = mock_cursor
+
+    # Simulamos o retorno do banco de dados
+    mock_cursor.fetchall.return_value = [
+        (1, "rua", "Um bairro ai", "Uma Cidade"),
+        (2, "rua", "Um bairro ai", "Uma Cidade"),
+    ]
+
+
+    # Substituímos a função `connect_db` para retornar nosso Mock em vez de uma conexão real
+    mock_conectando_db.return_value = mock_conn
+
+    # Fazemos a requisição para a API
+    response = client.get("/alunos")
+
+    # Verificamos se o código de status da resposta é 200 (OK)
     assert response.status_code == 200
-    filtrado = response.get_json()
-    assert filtrado["cidades"] == "Limeira"
+
+    # Verificamos se os dados retornados estão corretos
+    expected_response = {
+        "filtrado": [
+            {"id": 1, "bairro": "Um bairro ai", "cidade": "Uma Cidade"},
+            {"id": 2, "bairro": "Um bairro ai", "cidade": "Uma Cidade"},
+        ]
+    }
+    assert response.get_json() == expected_response["filtrado"]
